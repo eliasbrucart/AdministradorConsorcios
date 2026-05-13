1ee4546cc26a04ab176537799d6689641bfb36c7
eliasbrucart <eliasbrucart@hardgames.com.ar>
2026-05-13 11:20:52 -0300

    Added Administradora class

---
+++ src/entidades/Administradora.java
@@ -0,0 +1,28 @@
+package entidades;
+
+public class Administradora {
+    //agregamos edificio
+    private double entrada;
+    private double gastos;
+    //agregar unidades funcionaless
+    public Administradora(){
+
+    }
+
+    public void setEntrada(double entrada){
+        this.entrada = entrada;
+    }
+
+    public double getEntrada(){
+        return entrada;
+    }
+
+    public void setGastos(double gastos){
+        this.gastos = gastos;
+    }
+
+    public double getGastos(){
+        return gastos;
+    }
+
+}
