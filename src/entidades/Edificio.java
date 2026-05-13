90d418ef28e778e82c54af7ccdadd5b4f04686d2
eliasbrucart <eliasbrucart@hardgames.com.ar>
2026-05-13 11:36:32 -0300

    Added Edificio class

---
+++ src/entidades/Edificio.java
@@ -0,0 +1,16 @@
+package entidades;
+
+public class Edificio {
+    //componemos las unidades funcionales
+    private String direccion;
+    public Edificio(){
+
+    }
+    public void setDireccion(String direccion){
+        this.direccion = direccion;
+    }
+
+    public String getDireccion(){
+        return direccion;
+    }
+}
