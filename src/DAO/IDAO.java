4ff59e278705274612e37de41eaaf6fbcddfd90a
eliasbrucart <eliasbrucart@hardgames.com.ar>
2026-05-27 09:54:58 -0300

    Added DaoException for DAO Interface

--- src/DAO/IDAO.java
+++ src/DAO/IDAO.java
@@ -3,9 +3,9 @@ package DAO;
 import java.util.ArrayList;
 
 public interface IDAO <T>{ //Le pasamos un generico para que los metodos acepten todo tipo de dato.
-    public void agregar(T elemento);
-    public void eliminar(int id);
-    public void modificar(T elemento);
-    public void consultar(int id);
+    public void agregar(T elemento) throws DaoException;
+    public void eliminar(int id) throws DaoException;
+    public void modificar(T elemento) throws DaoException;
+    public void consultar(int id) throws DaoException;
     public ArrayList<T> consultarTodo();
 }
